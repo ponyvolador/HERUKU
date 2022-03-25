1d35458a3445697fe467dfa42a6464554b1c98c7
ponyvolador <liastar49@gmail.com>
2022-03-24 22:55:39 -0600

    Se agrega el proyecto

---
+++ pagina.php
@@ -0,0 +1,23 @@
+<head>
+<h1 style="color:white;">  PAGINA SENCILLA</h1>
+<link href="css/index_style.css" rel="stylesheet" type="text/css">
+<link href="css/estilos.css" rel="stylesheet" type="text/css">
+</head>
+<div style=”color : white;”>
+  <h2 style="color:white;">   <h2>
+  <h2 style="color:white;"> Pagina de prueba </h2>
+</div>
+<body>
+<div id="header">
+</div>
+ <h1 style="color:white;"> ENCLACE A VIDEO</h1>
+ <iframe width="560" height="315" 
+src="https://www.youtube.com/embed/M4BXTwLqNlM">
+</iframe>
+ </body>
+<footer>
+ <p style="color:white;">Copyright © Maria Amalia Salcedo Arenas/ 
+Conceptualización de servicios en la nube/
+Codigo:220966084/ 
+Correo:liastar49@gmail.com.</p>
+</footer>
